d405e0d4d2e109a242e4bf0da0c34846cab2c565
Richard Hambleton <hambleton.5@gmail.com>
2017-06-20 19:45:01 -0400

    added file

---
+++ getHost.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+content=$(curl -s -G -A "Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36" "https://www.google.com/search?q=inurl:blogspot+shoes")
+
+results=($(echo $content | 
+grep -Eoi '<a [^>]+>' | 
+grep -Eo 'href="[^\"]+"' | 
+grep -Eo '(http|https)://[^/"]+' |
+grep 'blogspot.com'))
+
+selectedresult=${results[$RANDOM % ${#results[@]} ]}
+
+echo -n "$selectedresult" > sourceHost.dat
